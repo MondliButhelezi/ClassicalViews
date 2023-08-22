e7c27d0b55f5a58936ca8f121ef16ef40517eb8f
Mondli Buthelezi <67345545+MondliButhelezi@users.noreply.github.com>
2023-08-22 18:19:07 +0200

    Classes

--- my_stations/myapp/urls.py
+++ my_stations/myapp/urls.py
@@ -1,16 +1,20 @@
 from django.urls import path
 from . import views
-from .views import error_404_view
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.Myapp.as_view(), name='myapp'),
-    path('monitoring_stations/', views.monitoring_stations_view, name='monitoring_stations'),
-    path('monitoring_stations/add/', views.add_monitoring_station, name='add_monitoring_station'),
-    path('monitoring_stations/<int:pk>/edit/', views.edit_monitoring_station, name='edit_monitoring_station'),
-    path('monitoring_stations/<int:pk>/delete/', views.delete_monitoring_station, name='delete_monitoring_station'),
-    path('equipment_type/', views.equipment_type_view, name='equipment_type'),
+    path('', views.MyappView.as_view(), name='myapp'),
+    path('monitoring_stations/', views.MonitoringStationsView.as_view(), name='monitoring_stations'),
+    path('monitoring_stations/add/', views.AddMonitoringStationView.as_view(), name='add_monitoring_station'),
+    path('monitoring_stations/<int:pk>/edit/', views.EditMonitoringStationView.as_view(), name='edit_monitoring_station'),
+    path('monitoring_stations/<int:pk>/delete/', views.DeleteMonitoringStationView.as_view(), name='delete_monitoring_station'),
+
+    path('equipment_type/', views.EquipmentTypeView.as_view(), name='equipment_type'),
+    path('equipment_type/add/', views.AddEquipmentTypeView.as_view(), name='add_equipment_type'),
+    path('equipment_type/<int:pk>/edit/', views.EditEquipmentTypeView.as_view(), name='edit_equipment_type'),
+    path('equipment_type/<int:pk>/delete/', views.DeleteEquipmentTypeView.as_view(), name='delete_equipment_type'),
+
 ]
 
 handler404 = 'myapp.views.error_404_view'
--- my_stations/myapp/views.py
+++ my_stations/myapp/views.py
@@ -2,64 +2,115 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import MonitoringStation, EquipmentType
 from django import forms
 from django.views import View
-from django.http import HttpResponse
 
 
-# Create your views here.
-
-class Myapp(View):
+class MyappView(View):
     def get(self, request):
         return render(request, 'myapp.html')
 
 
+class MonitoringStationsView(View):
+    def get(self, request):
+        monitoring_stations = MonitoringStation.objects.all()
+        return render(request, 'stations/monitoring_stations.html', {'object_list': monitoring_stations})
 
-def monitoring_stations_view(request):
-    monitoring_stations = MonitoringStation.objects.all()
-    return render(request, 'stations/monitoring_stations.html', {'object_list': monitoring_stations})
 
+class AddMonitoringStationView(View):
+    def get(self, request):
+        form = MonitoringStationForm()
+        return render(request, 'stations/add_monitoring_station.html', {'form': form})
 
-def add_monitoring_station(request):
-    if request.method == 'POST':
+    def post(self, request):
         form = MonitoringStationForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('monitoring_stations')
-    else:
-        form = MonitoringStationForm()
-    return render(request, 'stations/add_monitoring_station.html', {'form': form})
+        return render(request, 'stations/add_monitoring_station.html', {'form': form})
 
 
-def edit_monitoring_station(request, pk):
-    station = get_object_or_404(MonitoringStation, pk=pk)
-    if request.method == 'POST':
+class EditMonitoringStationView(View):
+    def get(self, request, pk):
+        station = get_object_or_404(MonitoringStation, pk=pk)
+        form = MonitoringStationForm(instance=station)
+        return render(request, 'stations/edit_monitoring_station.html', {'form': form})
+
+    def post(self, request, pk):
+        station = get_object_or_404(MonitoringStation, pk=pk)
         form = MonitoringStationForm(request.POST, instance=station)
         if form.is_valid():
             form.save()
             return redirect('monitoring_stations')
-    else:
-        form = MonitoringStationForm(instance=station)
-    return render(request, 'stations/edit_monitoring_station.html', {'form': form})
+        return render(request, 'stations/edit_monitoring_station.html', {'form': form})
 
 
-def delete_monitoring_station(request, pk):
-    station = get_object_or_404(MonitoringStation, pk=pk)
-    if request.method == 'POST':
+class DeleteMonitoringStationView(View):
+    def get(self, request, pk):
+        station = get_object_or_404(MonitoringStation, pk=pk)
+        return render(request, 'stations/delete_monitoring_station.html', {'station': station})
+
+    def post(self, request, pk):
+        station = get_object_or_404(MonitoringStation, pk=pk)
         station.delete()
         return redirect('monitoring_stations')
-    return render(request, 'stations/delete_monitoring_station.html', {'station': station})
 
 
-# forms
 class MonitoringStationForm(forms.ModelForm):
     class Meta:
         model = MonitoringStation
         fields = '__all__'
 
 
-def equipment_type_view(request):
-    equipment_type = EquipmentType.objects.all()
-    return render(request, 'equipment/equipment_type.html',  {'object_list': equipment_type})
+class EquipmentTypeForm(forms.ModelForm):
+    class Meta:
+        model = EquipmentType
+        exclude = ['last_update']
 
 
-def error_404_view(request, exception):
-    return render(request, '404.html', status=404)
+class EquipmentTypeView(View):
+    def get(self, request):
+        equipment_type = EquipmentType.objects.all()
+        return render(request, 'equipment/equipment_type.html', {'object_list': equipment_type})
+
+
+class EditEquipmentTypeView(View):
+    def get(self, request, pk):
+        equipment = get_object_or_404(EquipmentType, pk=pk)
+        form = EquipmentTypeForm(instance=equipment)
+        return render(request, 'equipment/edit_equipment_type.html', {'form': form})
+
+    def post(self, request, pk):
+        equipment = get_object_or_404(EquipmentType, pk=pk)
+        form = EquipmentTypeForm(request.POST, instance=equipment)
+        if form.is_valid():
+            form.save()
+            return redirect('equipment_type')
+        return render(request, 'equipment/edit_equipment_type.html', {'form': form})
+
+
+class DeleteEquipmentTypeView(View):
+    def get(self, request, pk):
+        equipment = get_object_or_404(EquipmentType, pk=pk)
+        return render(request, 'equipment/delete_equipment_type.html', {'equipment': equipment})
+
+    def post(self, request, pk):
+        equipment = get_object_or_404(EquipmentType, pk=pk)
+        equipment.delete()
+        return redirect('equipment_type')
+
+
+class AddEquipmentTypeView(View):
+    def get(self, request):
+        form = EquipmentTypeForm()
+        return render(request, 'equipment/add_equipment_type.html', {'form': form})
+
+    def post(self, request):
+        form = EquipmentTypeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('equipment_type')
+        return render(request, 'equipment/add_equipment_type.html', {'form': form})
+
+
+class Error404View(View):
+    def get(self, request, exception):
+        return render(request, '404.html', status=404)
